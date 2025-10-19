a023d41bc0676e313541bfd7d8b9435f8d698817
Maurício <gois.mauricio12@gmail.com>
2025-10-19 16:05:47 -0300

    Frontend do login pronto

--- src/pages/Login/Components/SideForm.jsx
+++ src/pages/Login/Components/SideForm.jsx
@@ -1,54 +1,94 @@
 import { Link } from 'react-router-dom'
-import LogoForm from '../../../assets/images/Logotipo.png'
+import { useState } from 'react';
 import { IoIosArrowBack, IoIosMail } from "react-icons/io";
+import { RiLockPasswordFill } from "react-icons/ri";
+import { FaEyeSlash } from "react-icons/fa";
+import { IoEyeSharp } from "react-icons/io5";
+import LogoForm from '../../../assets/images/Logotipo.png'
 import styles from '../styles.module.css'
 
 export default function SideForm() {
+    const [showPassword, setShowPassword] = useState(false);
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
+
     return (
         <>
-            <section className='w-[50%]'>
-                <Link to={'/'} className='flex justify-center items-center gap-2 font-inter font-bold text-lg fixed top-5 left-5'>
-                    <span className='text-primary text-xl bg-secundary rounded-full'>
+            <section className='w-full lg:w-[50%] h-screen flex flex-col items-baseline'>
+                <Link to={'/'} className='flex justify-center items-center gap-2 font-inter font-bold text-lg mt-5 ms-5'>
+                    <span className='text-white-custom text-xl bg-black-custom rounded-full'>
                         <IoIosArrowBack />
                     </span>
 
                     Voltar ao ínicio
                 </Link>
 
-                <section className='shadow-custom-sm w-[75%] m-auto p-5 rounded-lg'>
-                    <img src={LogoForm} alt="Logotipo VerSonhos" className='w-[50%] mx-auto mb-7' />
+                <section className='shadow-custom-sm w-[65%] m-auto px-10 py-5 rounded-lg'>
+                    <img src={LogoForm} alt="Logotipo VerSonhos" className='w-[65%] mx-auto mb-7' />
 
                     <div className='text-center font-inter mb-7'>
                         <h2 className='font-bold text-2xl mb-2'>Bem-vindo</h2>
-                        <p className='text-secundary-400 font-medium'>Entre na sua conta para continuar</p>
+                        <p className='text-black-custom-400 font-medium'>Entre na sua conta para continuar</p>
                     </div>
 
-                    <form>
-                        <div>
-                            <label htmlFor="">E-mail:</label>
-                            <input type="text" placeholder='sdkjshdhkj' name="" id="" />
+                    <form className='w-full flex flex-col justify-center items-center gap-5 mb-5'>
+                        <div className='w-full flex flex-col items-start justify-center gap-2'>
+                            <label htmlFor="emailLogin" className='text-black-custom-500 font-semibold'>E-mail:</label>
+                            
+                            <div className='w-full flex relative'>
+                                <IoIosMail className='text-xl text-black-custom-400 absolute left-2 top-2.5'/>
+                                <input type="email" placeholder='seu@email.com' name="emailLogin" id="emailLogin" className='bg-gray-100 border-gray-300 border-2 focus:border-tertiary outline-0 focus:shadow-tertiary transition ease-in-out rounded-md w-full py-1.5 ps-9 pe-4' />
+                            </div>
                         </div>
 
-                        <div>
-                            <label htmlFor="">Senha:</label>
-                            <input type="text" placeholder='sdkjshdhkj' name="" id="" />
+                        <div className='w-full flex flex-col items-start justify-center gap-2'>
+                            <label htmlFor="passwordLogin" className='text-black-custom-500 font-semibold'>Senha:</label>
+                            
+                            <div className="w-full flex relative">
+                                <RiLockPasswordFill className='text-xl text-black-custom-400 absolute left-2 top-2.5'/>
+                                <input type={showPassword ? "text" : "password"} placeholder='*********' name="passwordLogin" id="passwordLogin" className='bg-gray-100 border-gray-300 border-2 focus:border-tertiary outline-0 focus:shadow-tertiary transition ease-in-out rounded-md w-full py-1.5 ps-9 pe-4' />
+                                
+                                <button 
+                                    type="button"
+                                    onClick={togglePasswordVisibility} 
+                                    className='absolute right-2 top-2.5 cursor-pointer'
+                                    aria-label={showPassword ? "Esconder senha" : "Mostrar senha"}
+                                >
+                                    {showPassword ? (
+                                    <FaEyeSlash className='text-xl text-black-custom-400'/>
+                                    ) : (
+                                    <IoEyeSharp className='text-xl text-black-custom-400'/>
+                                    )}
+                                </button>
+                            </div>
                         </div>
 
-                        <p className='text-secundary-500'>
+                        <p className='text-black-custom-500'>
                             Esqueceu sua senha? 
                             <Link className='text-tertiary hover:text-tertiary-400 transition ease-in-out italic'> Recuperar aqui!</Link>
                         </p>
 
-                        <button className='bg-tertiary text-primary font-semibold w-[50%] py-0.5 rounded-sm cursor-pointer shadow-sm'>
+                        <button className='bg-tertiary hover:bg-tertiary-500 transition ease-in-out text-white-custom font-semibold w-[80%] py-0.5 mt-2 rounded-sm cursor-pointer shadow-sm'>
                             Entrar
                         </button>
                     </form>
 
-                    <p>Criar conta</p>
+                    <div className='text-center flex flex-col gap-5 text-black-custom-500'>
+                        <div className="flex items-center my-2">
+                            <div className="flex-grow border-t border-black-custom-400"></div>
+                            
+                            <p className="mx-4 whitespace-nowrap">
+                                Criar uma conta
+                            </p>
+                            
+                            <div className="flex-grow border-t border-black-custom-400"></div>
+                        </div>
 
-                    <p>
-                        Não tem uma conta? <Link className='text-center text-tertiary hover:text-tertiary-400 transition ease-in-out italic'>Criar conta</Link>
-                    </p>
+                        <p>
+                            Não tem uma conta? <Link className='text-center text-tertiary hover:text-tertiary-400 transition ease-in-out italic'>Criar conta</Link>
+                        </p>
+                    </div>
                 </section>
             </section>
         </>
--- src/pages/Login/Components/SideImage.jsx
+++ src/pages/Login/Components/SideImage.jsx
@@ -5,10 +5,10 @@ export default function SideImage() {
     return (
         <>
             <section 
-                className="w-[50%] flex justify-center items-end bg-cover bg-no-repeat bg-position-[center_top_-8rem] h-screen shadow-inner-contrast" 
+                className="w-[50%] hidden lg:flex justify-center items-end bg-cover bg-no-repeat bg-center h-[100vh] shadow-inner-contrast" 
                 style={{ backgroundImage: `url(${ImageBg})` }}
             >
-                <div className='w-full text-primary mb-20'>
+                <div className='w-full text-white-custom mb-20 text-shadow-lg'>
                     <h2 className='font-fredoka text-3xl font-semibold text-center w-[50%] mx-auto mb-4'>Onde a imaginação encontra a tecnologia.</h2>
                     <p className='font-inter text-2xl text-center'>Aprendizado imersivo com tecnologia</p>
                 </div>
