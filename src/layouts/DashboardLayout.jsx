9f7d912543441064834d0d83d72aacf3ee0754a6
Maurício <gois.mauricio12@gmail.com>
2025-10-29 21:18:21 -0300

    ScrollTop pronto para cada página

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -2,10 +2,12 @@ import Sidebar from '../components/SideBar/SideBar';
 import AccessibilityWidget from "@/components/AccessibilityWidget/AccessibilityWidget";
 import VLibras from '@djpfs/react-vlibras';
 import Chatbot from "../components/ChatBot/ChatBot";
+import ScrollToTop from '@/components/ScrollToTop/ScrollToTop.jsx'
 
 const DashboardLayout = ({ children }) => {
   return (
     <main className="flex min-h-screen">
+      <ScrollToTop />
       <Sidebar />
       <section className="flex-grow p-6 ms-20 bg-gray-100">
         {children}
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -3,10 +3,12 @@ import Footer from '../components/Footer/Footer';
 import AccessibilityWidget from "@/components/AccessibilityWidget/AccessibilityWidget";
 import VLibras from '@djpfs/react-vlibras';
 import Chatbot from "../components/ChatBot/ChatBot";
+import ScrollToTop from '@/components/ScrollToTop/ScrollToTop.jsx'
 
 const MainLayout = ({ children }) => {
   return (
     <main className="flex flex-col min-h-screen">
+      <ScrollToTop />
       <Header />
       <section className="flex-grow">
         {children}
