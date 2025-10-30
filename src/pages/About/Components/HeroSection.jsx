1203ee20c570a8923e2fcd5c6ac1126313835653
nicoelho <nicolas11coelho@gmail.com>
2025-10-29 22:16:38 -0300

    Att textos HeroSection about

--- src/pages/About/Components/HeroSection.jsx
+++ src/pages/About/Components/HeroSection.jsx
@@ -6,11 +6,11 @@ export default function HeroSection() {
       className="bg-cover bg-center bg-no-repeat h-[90vh] flex flex-col justify-center items-center text-center text-white px-6"
       style={{ backgroundImage: `url(${ImgHero})` }}
     >
-      <h1 className="text-6xl font-bold mb-4 font-fredoka">
+      <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold mb-4 font-fredoka">
         Quem <span className="text-blue-400">Somos?</span>
       </h1>
-      
-      <p className="max-w-2xl mx-auto font-inter text-3xl"> 
+
+      <p className="max-w-2xl mx-auto font-inter text-xl sm:text-2xl md:text-3xl">
         Somos um grupo de alunos do Instituto PROA, movidos pelo propósito de transformar momentos difíceis em experiências de esperança. Nosso projeto nasceu com a ideia de utilizar a tecnologia do óculos de realidade virtual (VR) para levar alegria, distração e novas vivências a crianças internadas em hospitais, principalmente aquelas em tratamento contra o câncer.
       </p>
     </section>
