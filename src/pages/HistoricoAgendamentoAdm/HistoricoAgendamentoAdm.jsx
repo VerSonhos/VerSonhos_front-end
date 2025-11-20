28e5001816669b45eacf2da45385abda811f858d
nicoelho <nicolas11coelho@gmail.com>
2025-11-20 01:09:07 -0300

    att remoção do comentário

--- src/pages/HistoricoAgendamentoAdm/HistoricoAgendamentoAdm.jsx
+++ src/pages/HistoricoAgendamentoAdm/HistoricoAgendamentoAdm.jsx
@@ -8,8 +8,6 @@ export default function historicoAgendamentoAdm(){
         <DashboardLayout>
             <HeaderCards/>
             <TableAgendamento/>
-
-        {/* hvhjvjh */}
         </DashboardLayout>
         </>
     );
