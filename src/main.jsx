53a9c4c235f2bf38b20bda6c3dd5af3cdeaca9e1
vitor-mota <vitormotanovais@gmail.com>
2025-11-20 15:06:41 -0300

    Testando ajuste no vlibras

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles/global.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles/global.css";
+import "./styles/vlibras-fix.css";
 
-import AppRoutes from './routes/AppRoutes';
+import AppRoutes from "./routes/AppRoutes";
+import VLibras from "@djpfs/react-vlibras";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <AppRoutes />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <>
+    <VLibras forceOnload={true} />
+
+    <StrictMode>
+      <AppRoutes />
+    </StrictMode>
+  </>
+);
