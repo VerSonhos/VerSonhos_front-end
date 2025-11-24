0330b378b6908e9e9624f47919b39619437c5240
nicoelho <nicolas11coelho@gmail.com>
2025-11-24 20:43:54 -0300

    Alterando espaçamento seção

--- src/pages/Visit/Components/Timeline.jsx
+++ src/pages/Visit/Components/Timeline.jsx
@@ -41,7 +41,7 @@ export const Timeline = ({ data }) => {
       ref={containerRef}
     >
       <div className="max-w-7xl mx-auto py-20 px-4 md:px-8 lg:px-10">
-        <h2 className="mb-4 text-black dark:text-white max-w-4xl font-fredoka text-4xl md:text-5xl font-extrabold">
+        <h2 className="mb-4 text-thirteenth dark:text-white max-w-4xl font-fredoka text-4xl md:text-5xl font-extrabold">
           Jornada VerSonhos: Transformando sonhos com Realidade Virtual
         </h2>
         <p className="text-neutral-700 dark:text-neutral-300 text-3xl md:text-2xl max-w-sm font-inter ">
