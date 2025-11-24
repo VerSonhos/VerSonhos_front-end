9efd641323bbc6aff82e2859bfe21c845a11b9ba
nicoelho <nicolas11coelho@gmail.com>
2025-11-24 20:34:26 -0300

    Upando linha do tempo

---
+++ src/pages/Visit/Components/TimelineData.jsx
@@ -0,0 +1,47 @@
+export const TimelineData = [
+  {
+    title: "✉️",
+    content: (
+      <div className="text-neutral-700 dark:text-neutral-300">
+        <h2 className="font-fredoka text-3xl font-bold">1. Contato e Agendamento</h2>
+        <p className="text-2xl">A instituição de saúde agenda a visita, fornecendo dados sobre as crianças, para definirmos data e hora da intervenção.</p>
+      </div>
+    ),
+  },
+  {
+    title: "📋",
+    content: (
+      <div className="text-neutral-700 dark:text-neutral-300">
+        <h2 className="font-fredoka text-3xl font-bold">2. Planejamento da Experiência</h2>
+        <p className="text-2xl">Com base nos dados, selecionamos e preparamos as experiências de Realidade Virtual e a logística de equipamentos e equipe para cada criança.</p>
+      </div>
+    ),
+  },
+  {
+    title: "🚚",
+    content: (
+      <div className="text-neutral-700 dark:text-neutral-300">
+        <h2 className="font-fredoka text-3xl font-bold">3. Caminho da Alegria</h2>
+        <p className="text-2xl">Nossa equipe chega à instituição com os equipamentos de Realidade Virtual, realiza a checagem final e coordena com a equipe médica para garantir um ambiente seguro.</p>
+      </div>
+    ),
+  },
+  {
+    title: "🚀",
+    content: (
+      <div className="text-neutral-700 dark:text-neutral-300">
+        <h2 className="font-fredoka text-3xl font-bold">4. Lançamento para a Imaginação</h2>
+        <p className="text-2xl">No grande momento, as crianças exploram mundos virtuais com a nossa equipe, interagindo e vivendo aventuras para esquecer a rotina hospitalar.</p>
+      </div>
+    ),
+  },
+  {
+    title: "✨",
+    content: (
+      <div className="text-neutral-700 dark:text-neutral-300">
+        <h2 className="font-fredoka text-3xl font-bold">5. O Legado de um Sorriso</h2>
+        <p className="text-2xl">Após a experiência, recolhemos os equipamentos, nos despedimos das crianças e realizamos um feedback com o hospital para avaliar o impacto e buscar melhorias.</p>
+      </div>
+    ),
+  },
+];
