6bc0789a7422f626ac8acea594585339fca12e1b
nicoelho <nicolas11coelho@gmail.com>
2025-12-11 17:11:40 -0300

    Responsividade histórico agendamento

--- src/pages/HistoricoAgendamentoAdm/Components/HeaderCards.jsx
+++ src/pages/HistoricoAgendamentoAdm/Components/HeaderCards.jsx
@@ -1,13 +1,28 @@
-export default function HeaderCards({number, text, color}) {
-    return (
-        <article className={`w-[80%] lg:w-[24%] font-fredoka transition hover:scale-102 cursor-default text-white font-semibold flex flex-col justify-center items-center gap-4 shadow-custom-sm py-8 rounded-xl border-2 border-quintenary ${color}`}>
-            <div className="text-3xl">
-                {number}
-            </div>
+export default function HeaderCards({ number, text, color }) {
+  return (
+    <article
+      className={`
+        w-[90%] sm:w-[80%] lg:w-[24%]
+        font-fredoka transition hover:scale-102
+        cursor-default text-white font-semibold
+        flex flex-col justify-center items-center
+        gap-4 shadow-custom-sm py-6 sm:py-8
+        rounded-xl border-2 border-quintenary
+        ${color}
+      `}
+    >
+      <div className="text-2xl sm:text-3xl">
+        {number}
+      </div>
 
-            <p className="text-2xl text-center px-4">
-                {text}
-            </p>
-        </article>
-    )
+      <p className="
+        text-base xs:text-lg sm:text-xl md:text-2xl
+        text-center
+        px-2 sm:px-4 
+        leading-snug break-words
+      ">
+        {text}
+      </p>
+    </article>
+  );
 }
